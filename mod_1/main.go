bf66d235ffa55350249c70fbcb4e569a98adcb63
elevante <elevante.a@mail.ru>
2023-02-09 02:07:50 +0300

    WIP: added a description of tasks and completed tasks 2, 3, 4

--- mod_1/main.go
+++ mod_1/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
 
 type HealthCheck struct {
 	ServiceId int
@@ -23,7 +27,46 @@ func GenerateCheck() (a []HealthCheck) {
 	return a
 }
 
+func NumsRepCheck(arr []int) bool {
+	m := 0
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				m++
+				break
+			}
+		}
+	}
+	if m >= 2 {
+		return true
+	}
+	return false
+}
+
+func SortSliceCheck(a []string) bool {
+	b := make([]string, len(a))
+	copy(b, a)
+	sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })
+	if reflect.DeepEqual(a, b) {
+		return true
+	}
+	return false
+}
+
+func CountQuantityRep(s string) {
+	runes := []rune(s)
+	m := make(map[rune]int)
+	for _, key := range runes {
+		m[key]++
+	}
+	for key, value := range m {
+		fmt.Println(string(key), " - ", value)
+	}
+}
+
 func main() {
+	//Task 1
+	fmt.Println("\tTask 1")
 	fmt.Println("Тут будет выведен идентификатор")
 	a := GenerateCheck()
 	for _, id := range a {
@@ -31,4 +74,22 @@ func main() {
 			fmt.Println(id.ServiceId)
 		}
 	}
+
+	//Task2
+	fmt.Println("\tTask 2")
+	var arr = []int{2, 1, 1, 1, 4, 5, 6}
+	fmt.Println(NumsRepCheck(arr))
+	fmt.Println()
+
+	//Task3
+	fmt.Println("\tTask 3")
+	l := []string{"C", "A", "B", "C"}
+	//sort.Slice(l, func(i, j int) bool { return l[i] < l[j] })
+	fmt.Println(SortSliceCheck(l))
+
+	//Task4
+	fmt.Println("\tTask 4")
+	strCheck := "съешь еще этих мягких французких булок, да выпей чаю"
+	fmt.Println(strCheck)
+	CountQuantityRep(strCheck)
 }
