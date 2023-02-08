b57f18832feafc20e9fcfca212137e0939f23b15
elevante <elevante.a@mail.ru>
2023-02-08 19:55:00 +0300

    WIP: finished task

---
+++ mod_1/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type HealthCheck struct {
+	ServiceId int
+	status    string
+}
+
+const (
+	PasStatus  = "pass"
+	FailStatus = "fail"
+)
+
+func GenerateCheck() (a []HealthCheck) {
+	for i := 0; i < 5; i++ {
+		if i%2 == 0 {
+			a = append(a, HealthCheck{i, PasStatus})
+		} else {
+			a = append(a, HealthCheck{i, FailStatus})
+		}
+	}
+	return a
+}
+
+func main() {
+	fmt.Println("Тут будет выведен идентификатор")
+	a := GenerateCheck()
+	for i, id := range a {
+		if i%2 == 0 {
+			fmt.Println(id)
+		}
+	}
+}
