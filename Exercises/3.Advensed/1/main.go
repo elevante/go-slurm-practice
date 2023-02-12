d1ac490347b9f1bfcf7e773ccf8af73cd98e8b58
elevante <elevante.a@mail.ru>
2023-02-13 02:43:48 +0300

    WIP: finished task 1 in mod 2 and refactoring tasks in mod 2 and added description for task 1 in mod 2

---
+++ Exercises/3.Advensed/1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const pi float64 = 3.14
+
+type Rectangle struct {
+	a float64
+	b float64
+}
+
+type Circle struct {
+	r float64
+}
+
+func newCircle(a float64) *Circle {
+	return &Circle{
+		r: a,
+	}
+}
+
+func newRectangle(a, b float64) *Rectangle {
+	return &Rectangle{
+		a: a,
+		b: b,
+	}
+}
+
+func (r *Rectangle) Area() float64 {
+	return r.a * r.b
+}
+
+func (c *Circle) Area() float64 {
+	return pi * math.Pow(c.r, 2)
+}
+
+func (r *Rectangle) Type() string {
+	return "rectangle"
+}
+
+func (c *Circle) Type() string {
+	return "circle"
+}
+
+func main() {
+	rectangleNew := newRectangle(2.0, 4.0)
+	circleNew := newCircle(2.0)
+	squareRectangle := rectangleNew.Area()
+	squareCircle := circleNew.Area()
+
+	fmt.Println("Square", rectangleNew.Type(), squareRectangle)
+	fmt.Println("Square", circleNew.Type(), squareCircle)
+
+}
