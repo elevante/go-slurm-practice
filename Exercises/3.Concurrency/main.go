255e074df38c809131ac4a7b8c04ce62f4b70c12
elevante <elevante.a@mail.ru>
2023-02-19 02:07:47 +0300

    WIP: finished task 1

---
+++ Exercises/3.Concurrency/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func foo() {
+	panic("Паника в foo()")
+}
+
+func main() {
+	defer func() {
+		err := recover()
+		fmt.Println(err)
+	}()
+	fmt.Println("Start")
+	foo()
+	fmt.Println("End")
+}
